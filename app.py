4af02ec0c80c0c55eaf663006ef5625fcee05663
lebozec <nicolas.lebozec@xelians.fr>
2023-11-17 16:24:23 +0100

    Initialisation de flask_socketio + mise à jour de la doc

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message', msg)
+    socketio.emit('message', msg)
+
+    
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
     
