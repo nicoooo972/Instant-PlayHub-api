813a86b306db25b20dd07f90a8730a1ac835ac21
lebozec <nicolas.lebozec@xelians.fr>
2024-07-07 17:10:04 +0200

    changement configuration docker + db

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
-# db.py
-
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Database:
@@ -10,16 +12,10 @@ class Database:
         self.user = self.db['users']
 
 
-# Configurez votre URI et le nom de la base de données
-DATABASE_URI = ('mongodb+srv://flowfloxper:uTT8TxEKhEsudDCt@cluster0.8dcb73o'
-                '.mongodb.net/')
-DATABASE_NAME = 'instantplayhub'
+DATABASE_URI = os.getenv("DATABASE_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+CLIENT = os.getenv("CLIENT")
 
-# Initialisez une instance de la classe Database pour être utilisée ailleurs
-# dans votre application
 db = Database(DATABASE_URI, DATABASE_NAME)
-
-# import pymongo
-
-# client = pymongo.MongoClient("localhost", 27017)
-# db = client.instantplayhub
+client = MongoClient(DATABASE_URI)
+db = client[CLIENT]
